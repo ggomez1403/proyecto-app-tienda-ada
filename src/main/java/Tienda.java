8f3f094ddbe3ff2c529bf2f909a2e665d3fc9787
ggomez1403 <geralgomez1403@gmail.com>
2023-09-24 15:15:47 -0500

    initial project variables

--- src/main/java/Tienda.java
+++ src/main/java/Tienda.java
@@ -1,5 +1,24 @@
 public class Tienda {
     public static void main(String[] args) {
-        System.out.println("Bienvenido a la tienda!");
+        System.out.println("\nBienvenido a la tienda!");
+        String productName = "Leche Entera Alpina";
+        String productDescription = "Bolsa X 1.100 ml";
+        String productCategory = "Lacteo/Huevos y refrigerados";
+        String productTag = "Leches";
+        double productPrice = 3.100;
+
+        String priceFormat = String.format("%.3f", productPrice);
+
+
+        System.out.println("\n-------------------------------------");
+        System.out.println("Detalle de producto");
+        System.out.println("-------------------------------------");
+        System.out.println("Categoria: " + productCategory);
+        System.out.println("Etiquetas: " + productTag);
+        System.out.println("-------------------------------------");
+        System.out.println("Nombre: " + productName);
+        System.out.println("Descripción: " + productDescription);
+        System.out.println("Precio: $" + priceFormat);
+        System.out.println("-------------------------------------");
     }
 }
