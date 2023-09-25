880677f40e3f974907a256fd7bf50cb34396e6d8
ggomez1403 <geralgomez1403@gmail.com>
2023-09-24 21:23:00 -0500

    option menu added

--- src/main/java/Tienda.java
+++ src/main/java/Tienda.java
@@ -1,24 +1,87 @@
+import java.util.Scanner;
+
 public class Tienda {
     public static void main(String[] args) {
-        System.out.println("\nBienvenido a la tienda!");
-        String productName = "Leche Entera Alpina";
-        String productDescription = "Bolsa X 1.100 ml";
-        String productCategory = "Lacteo/Huevos y refrigerados";
-        String productTag = "Leches";
-        double productPrice = 3.100;
-
-        String priceFormat = String.format("%.3f", productPrice);
-
-
-        System.out.println("\n-------------------------------------");
-        System.out.println("Detalle de producto");
-        System.out.println("-------------------------------------");
-        System.out.println("Categoria: " + productCategory);
-        System.out.println("Etiquetas: " + productTag);
-        System.out.println("-------------------------------------");
-        System.out.println("Nombre: " + productName);
-        System.out.println("Descripción: " + productDescription);
-        System.out.println("Precio: $" + priceFormat);
-        System.out.println("-------------------------------------");
+        runMenu();
     }
+
+    public static void runMenu(){
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do{
+            displayMenu();
+            choice = scanner.nextInt();
+            handleUserChoice(choice);
+        }while(choice != 8);
+    }
+
+    public static void displayMenu(){
+        System.out.println(
+                """
+                        |o|                         /////////////\\\\\\
+                        |o|                        (((((((((((((   \\\\\\
+                        |o|                        ))) ~~      ~~   (((
+                        |o|                        ((( (*)     (*)  )))
+                        |o|                        )))     <        (((\s
+                        |o|                        ((( '\\______/`   )))\s
+                        |o|                        )))\\___________/(((\s
+                        |o|                        (((   _)  (_    )))\s\s
+                        |o|                              /\\__/\\""");
+        System.out.println("±----------------------------------------±");
+        System.out.println("|   Administrador Mi Tienda de Barrio    |");
+        System.out.println("±----------------------------------------±");
+        System.out.println("1. Agregar producto                      |");
+        System.out.println("2. Eliminar producto                     |");
+        System.out.println("3. Actualizar producto                   |");
+        System.out.println("4. Ver todos los productos               |");
+        System.out.println("5. Buscar producto                       |");
+        System.out.println("6. Crear venta de producto               |");
+        System.out.println("7. Ver historial de ventas               |");
+        System.out.println("8. Salir                                 |");
+        System.out.println("±----------------------------------------±");
+        System.out.print("   Ingresa tu opción:    (1 - 8)  ");
+    }
+
+    public static void handleUserChoice(int choice){
+        switch (choice){
+            case 1 -> addProduct();
+            case 2 -> removeProduct();
+            case 3 -> updateProduct();
+            case 4 -> seeAllProducts();
+            case 5 -> searchProduct();
+            case 6 -> createProductSale();
+            case 7 -> viewSalesHistory();
+            case 8 -> System.out.println("Saliendo...");
+            default -> System.out.println("Opción invalida. Por favor intenta de nuevo.");
+        }
+    }
+
+    public static void addProduct(){
+        System.out.println("Producto añadido");
+    }
+
+    public static void removeProduct(){
+        System.out.println("Producto eliminado");
+    }
+
+    public static void updateProduct(){
+        System.out.println("Producto actualizado");
+    }
+
+    public static void searchProduct(){
+        System.out.println("Buscando producto");
+    }
+
+    public static void createProductSale() {
+        System.out.println("Venta de producto creada");
+    }
+
+    public static void viewSalesHistory(){
+        System.out.println("Historial de ventas");
+    }
+
+    public static void seeAllProducts(){
+        System.out.println("Listado de todos los productos");
+    }
+
 }
